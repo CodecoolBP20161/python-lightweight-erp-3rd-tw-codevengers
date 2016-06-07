b4fabb8d89dbd56b60b7ac6ef76c2b8318e589a7
Csyk <balipaulcsik@gmail.com>
2016-06-07 14:05:23 +0200

    bug fixed, update not works yet

--- tool_manager/tool_manager.py
+++ tool_manager/tool_manager.py
@@ -22,7 +22,7 @@ common = SourceFileLoader("common", current_file_path + "/../common.py").load_mo
 
 # start this manager by a menu
 def start_module():
-    table = data_manager.get_table_from_file("tools.csv")
+    table = data_manager.get_table_from_file(current_file_path + "/tools.csv")
     list_options = ["Show Table", "Add to table", "Remove from table", "Update table"]
     ui.print_menu("Tool manager menu", list_options, "Exit to main menu")
     inputs = ui.get_inputs(["Please enter a number: "], "")
@@ -45,26 +45,25 @@ def start_module():
         exit()
     else:
         raise KeyError("There is no such option.")
-    data_manager.write_table_to_file("customers.csv", table)
+    data_manager.write_table_to_file(current_file_path + "/tools.csv", table)
 
 
 # print the default table of records from the file
 
 def show_table(table):
-    table_str = ""
-    for item in range(len(table)):
-        table_str += str(table[item]) + ("\n")
-    return table_str
+    ui.print_table(table, ['ID', 'Name', 'Producer', 'Year', 'Durability'])
 
-
-# Ask a new record as an input from the user than add it to @table, than return @table
+#  Ask a new record as an input from the user than add it to @table, than return @table
 
 
 def add(table):
-    record = ui.get_inputs(["name: ", "producer: ", "year: ", "number of pieces: "], " ")
+    record = ui.get_inputs(["Name: ", "Producer: ", "Year: ", "Durability: "], " ")
     record.insert(0, common.generate_random(table))
     table.append(record)
-    data_manager.write_table_to_file("tools.csv", table)
+    for i in table:
+        for element, l in enumerate(i):
+            i[element] = str(l)
+    data_manager.write_table_to_file(current_file_path + "/tools.csv", table)
     return table
 
 
@@ -73,22 +72,22 @@ def add(table):
 
 def remove(table, id_):
     for item in range(len(table)):
-        list_in_list = table[item]
-        if id_ in list_in_list:
-            table.pop(item)  # delete element of a table
-    data_manager.write_table_to_file("tools.csv", table)
+        if table[item][0] == id_[0]:
+            del table[item]
+    data_manager.write_table_to_file(current_file_path + "/tools.csv", table)
     return table
 
 # Update the record in @table having the id @id_ by asking the new data from the user,
 # than return @table
+
+
 def update(table, id_):
     table = data_manager.get_table_from_file(table)
     for item in range(len(table)):
         list_in_list = table[item]
         if id_ in list_in_list:
-            record = ui.get_inputs(["name: ", "producer: ", "year: ", "durability: "], " ")
-
-    data_manager.write_table_to_file("tools.csv", table)
+            record = ui.get_inputs(["Name: ", "Producer: ", "year: ", "durability: "], " ")
+    data_manager.write_table_to_file(current_file_path + "/tools.csv", table)
     return table
 
 start_module()
@@ -97,6 +96,8 @@ start_module()
 
 # the question: Which items has not yet exceeded their durability ?
 # return type: list of lists (the inner list contains the whole row with their actual data types)
+
+
 def get_available_tools(table):
 
     # your code
