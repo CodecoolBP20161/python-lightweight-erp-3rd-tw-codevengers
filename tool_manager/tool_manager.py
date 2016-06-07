683e95c2fdd1c4d6647a93bb216f1f24b4a14117
Csyk <balipaulcsik@gmail.com>
2016-06-07 14:54:35 +0200

    update works

--- tool_manager/tool_manager.py
+++ tool_manager/tool_manager.py
@@ -22,30 +22,31 @@ common = SourceFileLoader("common", current_file_path + "/../common.py").load_mo
 
 # start this manager by a menu
 def start_module():
-    table = data_manager.get_table_from_file(current_file_path + "/tools.csv")
-    list_options = ["Show Table", "Add to table", "Remove from table", "Update table"]
-    ui.print_menu("Tool manager menu", list_options, "Exit to main menu")
-    inputs = ui.get_inputs(["Please enter a number: "], "")
-    option = inputs[0]
-    if option == 1:
-        show_table(table)
-    elif option == 2:
-        add(table)
-    elif option == 3:
-        id_ = ui.get_inputs(["Please enter an ID: "], "")
-        remove(table, id_)
-    elif option == 4:
-        id_ = ui.get_inputs(["Please enter an ID: "], "")
-        update(table, id_)
-    elif option == 5:
-        get_lowest_price_item_id(table)
-    elif option == 6:
-        get_items_sold_between(table, month_from, day_from, year_from, month_to, day_to, year_to)
-    elif option == 0:
-        exit()
-    else:
-        raise KeyError("There is no such option.")
-    data_manager.write_table_to_file(current_file_path + "/tools.csv", table)
+    while True:
+        table = data_manager.get_table_from_file(current_file_path + "/tools.csv")
+        list_options = ["Show Table", "Add to table", "Remove from table", "Update table"]
+        ui.print_menu("Tool manager menu", list_options, "Exit to main menu")
+        inputs = ui.get_inputs(["Please enter a number: "], "")
+        option = inputs[0]
+        if option == 1:
+            show_table(table)
+        elif option == 2:
+            add(table)
+        elif option == 3:
+            id_ = ui.get_inputs(["Please enter an ID: "], "")
+            remove(table, id_)
+        elif option == 4:
+            id_ = ui.get_inputs(["Please enter an ID: "], "")
+            update(table, id_)
+        elif option == 5:
+            get_lowest_price_item_id(table)
+        elif option == 6:
+            get_items_sold_between(table, month_from, day_from, year_from, month_to, day_to, year_to)
+        elif option == 0:
+            exit()
+        else:
+            raise KeyError("There is no such option.")
+        data_manager.write_table_to_file(current_file_path + "/tools.csv", table)
 
 
 # print the default table of records from the file
@@ -63,7 +64,6 @@ def add(table):
     for i in table:
         for element, l in enumerate(i):
             i[element] = str(l)
-    data_manager.write_table_to_file(current_file_path + "/tools.csv", table)
     return table
 
 
@@ -74,7 +74,6 @@ def remove(table, id_):
     for item in range(len(table)):
         if table[item][0] == id_[0]:
             del table[item]
-    data_manager.write_table_to_file(current_file_path + "/tools.csv", table)
     return table
 
 # Update the record in @table having the id @id_ by asking the new data from the user,
@@ -82,15 +81,18 @@ def remove(table, id_):
 
 
 def update(table, id_):
-    table = data_manager.get_table_from_file(table)
     for item in range(len(table)):
         list_in_list = table[item]
-        if id_ in list_in_list:
-            record = ui.get_inputs(["Name: ", "Producer: ", "year: ", "durability: "], " ")
-    data_manager.write_table_to_file(current_file_path + "/tools.csv", table)
-    return table
+        if id_[0] == list_in_list[0]:
+            record = ui.get_inputs(["Name: ", "Producer: ", "Year: ", "Durability: "], " ")
+            record.insert(0, id_[0])
+            table[item] = record
+            for i in table:
+                for element, l in enumerate(i):
+                    i[element] = str(l)
+            return table
 
-start_module()
+
 # special functions:
 # ------------------
 
@@ -112,3 +114,4 @@ def get_average_durability_by_manufacturers(table):
     # your code
 
     pass
+start_module()
